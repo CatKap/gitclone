6ada71a0b3a0d3dce041fa4f0580ab73fa042ba7
Ilya Dutov <ilya.kapitan55@gmail.com>
2026-01-14 16:15:18 +0300

    Add fast-fetch

--- gitclone.c
+++ gitclone.c
@@ -1,3 +1,4 @@
+#include "libgit2/include/git2/errors.h"
 #include <git2.h>
 #include <stdio.h>
 
@@ -15,7 +16,7 @@ static int transfer_progress_cb(
     void *payload)
 {
     fprintf(stderr,
-        "Received %u/%u objects (%u/%u deltas), %zu bytes\r",
+        " Received %u/%u objects (%u/%u deltas), %zu bytes\r",
         stats->received_objects,
         stats->total_objects,
         stats->indexed_deltas,
@@ -23,6 +24,53 @@ static int transfer_progress_cb(
         stats->received_bytes
     );
     return 0;
+}
+
+int fast_forward(git_repository *repo)
+{
+    int r;
+    git_reference *head = NULL;
+    git_reference *remote_ref = NULL;
+    git_object *target = NULL;
+
+    /* get repo head */
+    r = git_repository_head(&head, repo);
+    if (r < 0)
+        goto cleanup;
+
+    const char *branch = git_reference_shorthand(head);
+
+    /* refs/remotes/origin/<branch> */
+    char remote_ref_name[256];
+    snprintf(remote_ref_name, sizeof(remote_ref_name),
+             "refs/remotes/origin/%s", branch);
+
+    r = git_reference_lookup(&remote_ref, repo, remote_ref_name);
+    if (r < 0)
+        goto cleanup;
+
+    /* get object commit */
+    r = git_reference_peel(&target, remote_ref, GIT_OBJECT_COMMIT);
+    if (r < 0)
+        goto cleanup;
+
+    /* checkout on upstream commit */
+    git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
+    checkout_opts.checkout_strategy =
+        GIT_CHECKOUT_FORCE | GIT_CHECKOUT_RECREATE_MISSING;
+
+    r = git_checkout_tree(repo, target, &checkout_opts);
+    if (r < 0)
+        goto cleanup;
+
+    /* move to HEAD */
+    r = git_repository_set_head(repo, remote_ref_name);
+
+cleanup:
+    git_object_free(target);
+    git_reference_free(remote_ref);
+    git_reference_free(head);
+    return r;
 }
 
 int main(int argc, char **argv) {
@@ -49,8 +97,26 @@ fetch_opts.callbacks.transfer_progress = transfer_progress_cb;
         opts.checkout_branch = branch;
     }
 
-    int r = git_clone(&repo, argv[1], argv[2], &opts);
+    const char* url = argv[1];
+    const char* directory = argv[2];
+    
+    int r = git_clone(&repo, url, directory, &opts);
+    if(r == GIT_EEXISTS){
+      fprintf(stderr, "Repo exsists, trying to pull...\n");
+      
+      int error = git_repository_open(&repo, directory);
+      if (error < 0) {
+        const git_error *e = git_error_last();
+        printf("Error %d/%d: %s\n", error, e->klass, e->message);
+        exit(error);
+      }
+      
+      r = fast_forward(repo);
+    }
+
+    printf("%d\n", r); 
     if (r < 0) {
+
         const git_error *e = git_error_last();
         fprintf(stderr, "Error: %s\n", e && e->message ? e->message : "unknown\n");
     }
